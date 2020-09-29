5ff7f3aff3703e1dc461e0fb741c3374f53d05a2
spoorthyandhe <70134338+spoorthyandhe@users.noreply.github.com>
2020-09-29 18:25:35 +0530

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,31 @@
+class Sling{
+    constructor(bodyA,pointB)
+    {
+    var options={
+     bodyA:bodyA,
+     pointB:pointB,
+     stiffness:1,
+     angularStiffness:1,
+     length:250
+    }
+this.pointB=pointB;
+this.sling = Constraint.create(options);
+World.add(world, this.sling);
+    }
+    fly(){
+      this.rope.bodyA=null;
+    }
+
+    display(){
+        if (this.sling.bodyA) { 
+            var pointA = this.sling.bodyA.position; 
+            var pointB = this.pointB; 
+            push(); 
+            stroke(255,255,255); 
+            strokeWeight(3); 
+            line(pointB.x, pointB.y, pointA.x, pointA.y);
+             pop(); 
+        } 
+    
+    }
+}
